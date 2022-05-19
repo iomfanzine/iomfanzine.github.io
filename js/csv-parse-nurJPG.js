8c0e9fc5e8ecf4496ad5899b38bac1ff9f023c56
isaacMedina1 <isaacmedina.music@gmail.com>
2022-05-19 18:25:53 -0500

    bangbang-siteUpdate
    Se agregan todos los archivos de Bang Bang para continuar la Web

---
+++ js/csv-parse-nurJPG.js
@@ -0,0 +1,20 @@
+var file = '../assets/sortedBios.csv'
+var data;
+
+Papa.parse(file, {
+  header: true,
+  download: true,
+  dynamicTyping: true,
+  skipEmptyLines: true,
+  complete: function(results) {
+
+    // read image key & write to DOM
+    for (var i = 0; i < results.data.length; i++) {
+      data = results.data[i].image;
+      var e = document.getElementById('img' + i);
+      e.src = './images/' + data;
+      e.alt = data;
+      console.log(results.data[i].image);
+    }
+  }
+});
